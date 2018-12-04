1feb09aa79f53d0016f0e8b27104861ded0870fe
COPACOINT\MaxH <m.hennen@student.fontys.nl>
2018-12-04 16:50:57 +0100

    header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 
 import { AppComponent } from './components/app/app.component';
 import { HeaderComponent } from './components/header/header.component';
 
+import {MatToolbarModule} from '@angular/material';
+import {HomeComponent} from './components/home/home.component';
+
+const routes: Routes = [
+  {path: '', component: HomeComponent}
+];
+
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatToolbarModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
